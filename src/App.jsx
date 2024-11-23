d68840fd41f97e6df31d8477a7f792235f6664f2
tung555 <132122215+tung555@users.noreply.github.com>
2024-11-23 00:25:44 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,42 @@ import HomePage from './pages/HomePage/HomePage';
 import MoviePage from './pages/MoviePage/Movie';
 import TVShowPage from './pages/TVShowPage/TVShow';
 import FavoritesPage from './pages/FavoritesPage/FavoritesList';
+import ListDetailsPage from './pages/ListDetails/ListDetails';
 import LoginPage from './pages/LoginPage/Login';
 import RegisterPage from './pages/RegisterPage/SignUp';
 import MovieDetail from './pages/MovieDetail/Detail';
+import PrivateRoute from './components/PrivateRoute'; 
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public Pages */}
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviePage />} />
         <Route path="/tvshows" element={<TVShowPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/favorites"
+          element={
+            <PrivateRoute>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/favorites/:id"
+          element={
+            <PrivateRoute>
+              <ListDetailsPage />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Dynamic Movie Details Page */}
         <Route path="/movies/:title" element={<MovieDetail />} />
       </Routes>
     </Router>
