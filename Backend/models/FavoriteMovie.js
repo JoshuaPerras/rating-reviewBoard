483aafadd879cb04d447aaf364f6b995bd9688c3
tung555 <132122215+tung555@users.noreply.github.com>
2024-11-25 21:12:47 -0500

    Update FavoriteMovie.js

--- Backend/models/FavoriteMovie.js
+++ Backend/models/FavoriteMovie.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const FavoriteMovieSchema = new mongoose.Schema({
   list_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'FavoriteList' },
   movie_id: { type: String, required: true }, // External Movie ID (e.g., from an API like TMDb)
+  movie_name: {type: String, required: true},
+  poster_uri: {type: String, required: true},
 });
 
 const FavoriteMovie = mongoose.model('FavoriteMovie', FavoriteMovieSchema);
